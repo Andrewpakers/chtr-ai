42e0611eaa2481af0b7e10c6698a20bdc6cf8f95
Andrew Akers <andrewpakers@gmail.com>
2023-05-25 14:35:57 -0400

    Changed code to reproduce bug

--- components/chatPicker.js
+++ components/chatPicker.js
@@ -4,12 +4,10 @@ import { getAllChats, getMessages } from '../utils/storageManager';
 
 
 
-function ChatListBuilder({ chatrooms, setDeclareActiveChat }) {
-
+function ChatListBuilder({ chatrooms, setTempActiveChat }) {
     const chattiles = chatrooms?.map((chatroom) => {
-
         return (
-            <li key={chatroom?.id} onClick={() => setDeclareActiveChat(chatroom?.name)} >
+            <li key={chatroom?.id} onClick={() => setTempActiveChat(chatroom?.name)} >
                 <ChatroomTile title={chatroom?.name} lastMessage={chatroom?.message} />
             </li>
         );
@@ -22,6 +20,8 @@ function ChatListBuilder({ chatrooms, setDeclareActiveChat }) {
     );
 }
 
+//retrieves list of chatrooms from DB, then queries the latest message in each room
+//Then creates an object with that information that can used to render ChatTiles
 async function updateChatroomList(setChatrooms) {
     const chatlistObjs = [];
     const chatrooms = await getAllChats();
@@ -40,19 +40,42 @@ async function updateChatroomList(setChatrooms) {
 
 export default function ChatPicker({ setActiveChat }) {
     const [chatrooms, setChatrooms] = useState([]);
-    const [declarActiveChat, setDeclareActiveChat] = useState("");
+    const [tempActiveChat, setTempActiveChat] = useState("");
 
     useEffect(() => {
-        updateChatroomList(setChatrooms);
+        // remove line below and uncomment other line to simulate db retrieval
+        // updateChatroomList(setChatrooms);
+        setChatrooms(mockChatrooms)
     }, []);
     useEffect(() => {
-        setActiveChat(declarActiveChat);
-        console.log(declarActiveChat);
-    }, [declarActiveChat])
+        // This is a workaround for a bug that occurs when trying to setActiveChat directly onClick
+        // I'm not sure why this works, but it does
+        setActiveChat(tempActiveChat);
+    }, [tempActiveChat])
 
     return (
-        <div id='chat-picker' className='w-[300px] min-w-[300px] h-[600px] border-r-2 border-slate-200'>
-            <ChatListBuilder setDeclareActiveChat={setDeclareActiveChat} chatrooms={chatrooms} />
+        <div className='w-[300px] min-w-[300px] h-[600px] border-r-2 border-slate-200'>
+            {/* to reproduce bug, set setTempActiveChat prop to setActiveChat */}
+            <ChatListBuilder setTempActiveChat={setTempActiveChat} chatrooms={chatrooms} />
         </div>
     );
 }
+
+//Mock chatroom data
+ const mockChatrooms = [
+    {
+        "name": "test-room-1",
+        "message": 29,
+        "id": "uWTSTbvZvPt3myAST96x"
+    },
+    {
+        "name": "test-room-2",
+        "message": "this is the second chat room",
+        "id": "testroom-message-2"
+    },
+    {
+        "name": "test room 3",
+        "message": "This is a super long message so that we make sure the string is properly truncated",
+        "id": "test-message-42"
+    }
+]
